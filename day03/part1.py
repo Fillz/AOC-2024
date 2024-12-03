af3690555aa249eec2925f31d7734e6cccaa4b9e
Filip Åsberg <filip.asberg@gmail.com>
2024-12-03 06:50:19 +0100

    Complete day 3

---
+++ day03/part1.py
@@ -0,0 +1,16 @@
+import re
+
+lines = open(0).read().splitlines()
+input = "".join(lines)
+
+pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
+
+matches = re.findall(pattern, input)
+
+res = 0
+
+for match in matches:
+    res += int(match[0]) * int(match[1])
+
+print(res)
+
